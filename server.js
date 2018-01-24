668da24e79f0d30059acbfff5dc70a56dbd75ba8
Dawn Suggs <dawnsuggs@Dawns-MacBook-Air.local>
2018-01-24 12:55:02 -0800

    Server_Branch

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require("express");
 const bodyParser = require ("body-parser");
 const path = require("path");
 // I added var router 
-var router = express.Router();
-const PORT = process.env.PORT || 3001;
+const router = express.Router();
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 //Outside now we should configure the API to use bodyParser and look for 
@@ -12,8 +12,8 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Serve up static assets (usually on heroku) removed: process.env.NODE_ENV === "production" from if statement
-if (true) {
+// Serve up static assets (usually on heroku) removed: process.env.NODE_ENV === "production" from if statement and inserted true
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
 }
 
